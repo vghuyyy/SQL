37eac99094ec80c8416d97e6ae67791305edc6b4
vghuyyy <huycon221102@gmail.com>
2024-05-22 23:22:00 +0700

    Bài_tập_buổi_9.sql

--- Bài_tập_buổi_9.sql
+++ Bài_tập_buổi_9.sql
@@ -17,3 +17,30 @@ when x+y>z then 'Yes'
 else 'No'
 end as triangle
 from Triangle
+
+-- ex3
+SELECT 
+ROUND(CAST(SUM(
+CASE 
+WHEN call_category = 'n/a' OR call_category IS NULL THEN 1 
+END) as decimal) / COUNT(*) * 100, 1) AS percentage_uncategorized
+FROM callers
+
+-- ex4
+select name
+from Customer
+where coalesce(referee_id,'1') <>2
+
+-- ex5
+select survived,
+count(case
+when pclass = 1 then 'first_class'
+end),
+count(case
+when pclass = 2 then 'second_class'
+end),
+count(case
+when pclass = 3 then 'third_class'
+end)
+from titanic
+group by survived
