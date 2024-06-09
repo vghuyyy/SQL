63d2ed38180b4e8867ea7d409bbd4893537bd8fe
vghuyyy <huycon221102@gmail.com>
2024-06-09 23:07:24 +0700

    project_2_2.sql

--- project_2_2.sql
+++ project_2_2.sql
@@ -12,7 +12,7 @@ on o.user_id = ot.user_id
 join bigquery-public-data.thelook_ecommerce.products as p
 on ot.product_id = p.id
 group by format_date('%y-%m', o.created_at),format_date('%y', o.created_at),p.category)
-
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 select 
 Month, --1
 Year, --2
@@ -26,3 +26,34 @@ TPV - Total_cost as Total_profit, --9
 (TPV - Total_cost)/Total_cost as Profit_to_cost_ratio --10
 from a
 order by Month, Year, Product_category
+
+with a as(select o.user_id, o.status, o.created_at, o.num_of_item, ot.sale_price from bigquery-public-data.thelook_ecommerce.orders o
+join bigquery-public-data.thelook_ecommerce.order_items as ot
+on o.user_id = ot.user_id
+where o.status = 'Complete'),
+
+b as (select user_id, amount,format_date('%Y-%m', first_date)as cohort_date, created_at,
+(extract(year from created_at) - extract(year from first_date))*12 
++ (extract(month from created_at) - extract(month from first_date)) +1 as index from (select user_id, num_of_item * sale_price as amount, min(created_at) over(partition by user_id) as first_date, created_at from a)b
+order by user_id),
+
+c as(select cohort_date, index, count(distinct user_id) as count, sum(amount) as revenue from b
+group by cohort_date, index),
+
+d as (select cohort_date,
+	sum(case when index = 1 then count else 0 end) as m1,
+	sum(case when index = 2 then count else 0 end) as m2,
+	sum(case when index = 3 then count else 0 end) as m3,
+  sum(case when index = 4 then count else 0 end) as m4
+from c
+group by cohort_date
+order by cohort_date)
+
+select 
+cohort_date,
+round(m1*100.0/m1,2) || '%' as m1,
+round(m2*100.0/m1,2) || '%' as m2,
+round(m3*100.0/m1,2) || '%' as m3,
+round(m4*100.0/m1,2) || '%' as m4
+from d
+
